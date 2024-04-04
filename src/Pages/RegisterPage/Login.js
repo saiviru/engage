82b84ba043598c5212ef28785b9443e4964306a8
Sai Kiran <saikiran.boga@sagarsoft.in>
2024-04-04 18:30:49 +0530

    login added

--- src/Pages/RegisterPage/Login.js
+++ src/Pages/RegisterPage/Login.js
@@ -1,106 +1,143 @@
-import React, { useEffect, useState } from 'react'
-import {FiMail} from "react-icons/fi"
-import {RiLockPasswordLine} from "react-icons/ri"
-import "../RegisterPage/RegisterPage.css"
-import { Link, useNavigate } from 'react-router-dom'
+import React,{useState} from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
-
-const Login = () => {
-    const navigate =useNavigate()
-    const [error,setError] =useState({})
-    const [submit,setSubmit] =useState(false)
-  
-    const [data,setData] =useState({
-        email:"",
-        password:"",
-    })
-
-    const handleChange=(e)=>{
-        const newObj={...data,[e.target.name]:e.target.value}
-        setData(newObj)
-    }
- 
-
-    const handleSignUp=(e)=>{
-        e.preventDefault()
-       setError(validationLogin(data))
-       setSubmit(true)
-    }
-
-    useEffect(()=>{
-        if(Object.keys(error).length === 0 && submit){
-            navigate("/home")
-        }
-    },[error])
+import { getUserById } from '../api/userService';
 
 
 
-   function validationLogin(data){
-        const error ={}
+import {
+  addUser,
+  loginSuccess,
+  logout,
+} from "../Store/reducers/loginReducer";
 
-        const emailPattern= /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-        const passwordPattern= /^[a-zA-Z0-9!@#\$%\^\&*_=+-]{8,12}$/g;
-
-        if(data.email === ""){
-            error.email ="* Email is Required"
-        }
-        else if(!emailPattern.test(data.email)){
-            error.email="* Email did not match"
-        }
-
-        
-        if(data.password === ""){
-            error.password = "* Password is Required"
-        }
-        else if(!passwordPattern.test(data.password)){
-            error.password="* Password not valid"
-        }
-        
-        return error
-   }
-
-
-
+function Copyright(props) {
   return (
-    <div className="container">
-        <div className="container-form">
-            <form onSubmit={handleSignUp}>
-                <h1>Login</h1>
-                <p>Please sign in to continue.</p>
-                <div className="inputBox">
-                    <FiMail className='mail'/>
-                    <input type="email" 
-                            name="email" 
-                            id="email" 
-                            onChange={handleChange}
-                            placeholder='Email'/> 
-                </div>
-                {error.email && <span style={{color:"red",display:"block",marginTop:"5px"}}>{error.email}</span>}
-
-                <div className="inputBox">
-                    <RiLockPasswordLine className='password'/>
-                    <input type="password" 
-                            name="password" 
-                            id="password" 
-                            onChange={handleChange}
-                            placeholder='Password'/>
-                </div>
-                {error.password && <span style={{color:"red",display:"block",marginTop:"5px"}}>{error.password}</span>}
-               
-
-                <div className='divBtn'>
-                    <small className='FG'>Forgot Password?</small>
-                    <button type='submit' className='loginBtn'>LOGIN</button>
-                </div>
-                
-            </form>
-
-            <div className='dont'>
-                <p>Don't have an account? <Link to="/signup"><span>Sign up</span></Link></p>
-            </div>
-        </div>
-    </div>
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://sagarsoft.in/">
+        Sagarsoft
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Login
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+
+  const [user, setUser] = useState(null);
+
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  const handleSuccess = async (credential) => {
+    const decoded = jwtDecode(credential.credential);
+    if (decoded) {
+      const requestBody = {
+        email: decoded.email,
+      };
+
+      const fetchData = async () => {
+        try {
+          const response = await getUserById(requestBody);
+          setUser(response.data);
+          if (result.data.status === "success") {
+            dispatch(loginSuccess());
+            dispatch(addUser(response.data));
+            navigate("/dashboard");
+          } else {
+            dispatch(logout());
+          }
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      };
+  
+      fetchData();
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h3">
+            Engage
+          </Typography>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          {/* <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box> */}
+          <GoogleLogin onSuccess={handleSuccess} onError={() => {}} />
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
